3eca29cc2e9bc225cf4872f16c6299de9eef09db
Russell Owen <rowen@uw.edu>
2020-09-29 17:45:11 -0700

    Add noreconnect and command-port arguments to bin/run_mock_tma.py
    since MTMountCsc now calls that to run the mock controller,
    and it needs control over those arguments.

--- bin/run_mock_tma.py
+++ bin/run_mock_tma.py
@@ -23,26 +23,47 @@ import logging
 
 from lsst.ts import MTMount
 
+logging.basicConfig()
+
 
 async def amain():
     parser = argparse.ArgumentParser("Simulate Tekniker's TMA")
+    parser.add_argument(
+        "--command-port",
+        type=int,
+        default=MTMount.CSC_COMMAND_PORT,
+        help="TCP port for commands.",
+    )
     parser.add_argument(
         "--log-level",
         type=int,
         default=logging.INFO,
         help="Log level (DEBUG=10, INFO=20, WARNING=30).",
     )
+    parser.add_argument(
+        "--noreconnect",
+        action="store_true",
+        help="Shut down when the the CSC disconnects?",
+    )
     namespace = parser.parse_args()
     log = logging.getLogger("TMASimulator")
     log.setLevel(namespace.log_level)
-    print(f"Mock TMA controller: command_port={MTMount.CSC_COMMAND_PORT}")
-    mock_controller = MTMount.mock.Controller(
-        command_port=MTMount.CSC_COMMAND_PORT, log=log, reconnect=True,
+    print(
+        f"Mock TMA controller: command_port={namespace.command_port}"
+        f"; reconnect={not namespace.noreconnect}"
     )
-    print("Mock TMA controller starting")
-    await mock_controller.start_task
-    print("Mock TMA controller running")
-    await asyncio.Future()  # Wait forever.
+    mock_controller = MTMount.mock.Controller(
+        command_port=namespace.command_port,
+        log=log,
+        reconnect=not namespace.noreconnect,
+    )
+    try:
+        print("Mock TMA controller starting")
+        await mock_controller.start_task
+        print("Mock TMA controller running")
+        await mock_controller.done_task
+    except asyncio.CancelledError:
+        pass
 
 
 asyncio.run(amain())
