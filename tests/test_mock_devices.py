1500415611275145910432240131ff87141389f6
Russell Owen <rowen@uw.edu>
2021-01-28 15:08:50 -0800

    Fix "This file is part of..." boilerplate

--- tests/test_mock_devices.py
+++ tests/test_mock_devices.py
@@ -1,4 +1,4 @@
-# This file is part of ts_ATDomeTrajectory.
+# This file is part of ts_MTMount.
 #
 # Developed for the LSST Telescope and Site Systems.
 # This product includes software developed by the LSST Project
