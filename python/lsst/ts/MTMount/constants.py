91e2f597223a2b5135df35a454f1aa7f0410d9bd
Russell Owen <rowen@uw.edu>
2021-02-08 09:19:16 -0800

    Update the CSC port with the correct value
    and remove unused constant EUI_COMMAND_PORT.
    I am keeping HHD_COMMAND_PORT because we can connect to it at need.

--- python/lsst/ts/MTMount/constants.py
+++ python/lsst/ts/MTMount/constants.py
@@ -20,9 +20,8 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __all__ = [
-    "EUI_COMMAND_PORT",
-    "HHD_COMMAND_PORT",
     "CSC_COMMAND_PORT",
+    "HHD_COMMAND_PORT",
     "TELEMETRY_PORT",
     "MIRROR_COVER_DRIVES",
     "AxisStateDict",
@@ -30,12 +29,8 @@ __all__ = [
 ]
 from lsst.ts.idl.enums.MTMount import AxisState, DriveState
 
-# Reply ports are one larger.
-EUI_COMMAND_PORT = 60005
+CSC_COMMAND_PORT = 30005
 HHD_COMMAND_PORT = 40005
-# An arbitrary value; update when Tekniker adds a dedicated
-# pair of ports for the CSC.
-CSC_COMMAND_PORT = 50005
 
 TELEMETRY_PORT = 50035
 
