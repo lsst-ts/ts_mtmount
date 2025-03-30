ef322c170dce6c1249ecd2a645da76bb298a785e
Michael Reuter <mareuter@lsst.org>
2025-03-29 19:40:05 -0700

    Replace all idl references with xml.

--- python/lsst/ts/mtmount/mock/main_axes_power_supply_device.py
+++ python/lsst/ts/mtmount/mock/main_axes_power_supply_device.py
@@ -23,7 +23,7 @@ __all__ = ["MainAxesPowerSupplyDevice"]
 
 
 from lsst.ts import utils
-from lsst.ts.idl.enums.MTMount import System
+from lsst.ts.xml.enums.MTMount import System
 
 from .base_device import BaseDevice
 
